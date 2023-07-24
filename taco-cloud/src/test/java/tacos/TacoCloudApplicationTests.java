f3f547d3067e496f4cdd81bf4993fced7cb2b150
LoretaVitina <56500810+LoretaVitina@users.noreply.github.com>
2023-07-24 19:52:33 +0300

    not working

---
+++ taco-cloud/src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,27 @@
+// package tacos;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class TacoCloudApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TacoCloudApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
