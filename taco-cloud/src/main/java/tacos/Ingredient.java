2f7afa48342a20c89a6d4da8ade082f1fe0dfe22
LoretaVitina <56500810+LoretaVitina@users.noreply.github.com>
2023-07-24 20:15:55 +0300

    working code

--- taco-cloud/src/main/java/tacos/Ingredient.java
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -6,10 +6,9 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
